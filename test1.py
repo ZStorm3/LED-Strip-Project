04bd1c5ac364ada212c7683aa0ea217ef22b6af3
ZStorm3 <145857993+ZStorm3@users.noreply.github.com>
2024-01-11 19:29:29 -0500

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,40 @@
+#!/user/bin/env python3
+
+import time
+from rpi_ws281x import *
+import board
+import argparse
+import neopixel
+
+# LED strip configuration:
+LED_COUNT      = 144      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 65     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+#Initialise a strips variable, provide the GPIO Data Pin
+#utilised and the amount of LED Nodes on strip and brightness (0 to 1 value)  
+pixels1 = neopixel.NeoPixel(board.D18, 144, brightness=1)
+
+n = 0
+
+def oneledanimation(n):
+    
+    while n < LED_COUNT:
+        pixels1[n] = (0, 0, 255)
+        n = n + 1
+        time.sleep(0.01)
+        pixels1[n-1] = (0, 0, 0)
+        
+        while n == LED_COUNT:
+            pixels1.fill((86,86,86))
+            time.sleep(3)
+            pixels1.fill((0, 0, 0))
+            quit()
+                    
+#executions
+oneledanimation(n)
