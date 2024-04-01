9963238834168b958a0eb386112058180a8bcf0c
ZStorm3 <145857993+ZStorm3@users.noreply.github.com>
2024-03-31 22:38:05 -0400

    Update test1.py
    Spelling error

--- test1.py
+++ test1.py
@@ -16,7 +16,7 @@ LED_BRIGHTNESS = 65     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
-#Initialise a strips variable, provide the GPIO Data Pin
+#Initialize a strips variable, provide the GPIO Data Pin
 #utilised and the amount of LED Nodes on strip and brightness (0 to 1 value)  
 pixels1 = neopixel.NeoPixel(board.D18, 144, brightness=1)
 
